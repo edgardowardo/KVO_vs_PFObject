6f091d8445b1c4196065637d085ea61c76ee0279
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air.local>
2015-07-31 16:30:10 +0100

    error replicated 'The class KVO_vs_PFObject.MyModel must be registered with registerSubclass before using Parse.'

--- KVO_vs_PFObject/ViewController.swift
+++ KVO_vs_PFObject/ViewController.swift
@@ -1,25 +1,42 @@
-//
-//  ViewController.swift
-//  KVO_vs_PFObject
-//
-//  Created by EDGARDO AGNO on 31/07/2015.
-//  Copyright (c) 2015 EDGARDO AGNO. All rights reserved.
-//
-
 import UIKit
+import Parse
+
+class MyModel : PFObject, PFSubclassing {
+    static func parseClassName() -> String {
+        return "MyModel"
+    }
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            self.registerSubclass()
+        }
+    }
+    @NSManaged var property1 : String?
+}
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var myObject = MyModel()
+        myObject.addObserver(self, forKeyPath: "property1", options: .New, context: nil)
+        myObject.property1 = "Hello"
+        myObject.removeObserver(self, forKeyPath: "property1")
+        
+        // If I comment these 4 lines. myObject is happy observing the property.
+        var anotherObject = MyModel()
+        anotherObject.addObserver(self, forKeyPath: "property1", options: .New, context: nil)
+        anotherObject.property1 = "World"
+        anotherObject.removeObserver(self, forKeyPath: "property1")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        var n : AnyObject? = change["new"]
+        switch keyPath {
+        case "property1" :
+            println("observed MyModel.property1 with value \(n)")
+        default :
+            break
+        }
     }
-
-
 }
 
